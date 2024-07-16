a11ab47852e61f3badbf4da0b367818c86387109
mccampero <mccampero2@gmail.com>
2024-07-16 13:29:11 -0400

    jsx fundamentals

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -3,7 +3,11 @@
 const App = () => {
 
   return (
-    <h1>Hello, friend!</h1>
+    <>
+    <h1 className="greeting">Hello, friend!</h1>
+    <h2>Hello, Universe</h2>
+    <hr/>
+    </>
   );
 };
 
